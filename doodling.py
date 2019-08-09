a9ea44baaf7fa4b811172beac40143dccaa34b17
Dan Wigglesworth <dwiggles@kewbeach.ca>
2019-08-09 07:02:47 -0400

    The end of the beginning
    The basic transforms including rotation work fine.

--- doodling.py
+++ doodling.py
@@ -1,6 +1,8 @@
 
 from tkinter import *
 from random import *
+import sys
+from os import listdir, remove
 
 tk = Tk()
 
@@ -27,11 +29,6 @@ def arb_triangle():
    #  return the_points
    return [ A, B, C ]
 
-print(unit_triangle())
-print("1:", unit_triangle()[0])
-print("2:", unit_triangle()[1])
-print("3:", 100*unit_triangle()[2][0], 100*unit_triangle()[2][1])
-
 def render_triangle(triangle=unit_triangle()):
     # render a triangle
     canvas.create_line(triangle[0][0],triangle[0][1],triangle[1][0],triangle[1][1])
@@ -41,30 +38,70 @@ def render_triangle(triangle=unit_triangle()):
 def translate_triangle(triangle=unit_triangle(), translation=[0,0]):
     # translate it
     return [
-        [triangle[0][0] + translation[0], triangle[0][1] + translation[1]], 
-        [triangle[1][0] + translation[0], triangle[1][1] + translation[1]], 
-        [triangle[2][0] + translation[0], triangle[2][1] + translation[1]] 
+        [triangle[0][0] + translation[0], triangle[0][1] + translation[1]],
+        [triangle[1][0] + translation[0], triangle[1][1] + translation[1]],
+        [triangle[2][0] + translation[0], triangle[2][1] + translation[1]]
         ]
 
 def dilate_triangle(triangle=unit_triangle(), dilation=[1,1]):
     # dilate it relative to the viewport origin
     return [
-        [triangle[0][0] * dilation[0], triangle[0][1] * dilation[1]], 
-        [triangle[1][0] * dilation[0], triangle[1][1] * dilation[1]], 
-        [triangle[2][0] * dilation[0], triangle[2][1] * dilation[1]] 
-        ]
-	     
-def rotate_triangle(triangle, rotate=1, rotate_origin=[0,0]):
-    # rotate it -- around the origin (Modify this to rotate around any specified point)
-    # this will be no "general" rotation, but instead it will be specifically a quarter turn
-    # it will be a quarter turn multiplied by 1, 2 or 3. 
-    # First a single quarter turn
-    return [
-        [triangle[0][1] * -1, triangle[0][0] * 1], 
-        [triangle[1][1] * -1, triangle[1][0] * 1], 
-        [triangle[2][1] * -1, triangle[2][0] * 1]
+        [triangle[0][0] * dilation[0], triangle[0][1] * dilation[1]],
+        [triangle[1][0] * dilation[0], triangle[1][1] * dilation[1]],
+        [triangle[2][0] * dilation[0], triangle[2][1] * dilation[1]]
         ]
 
+def rotate_triangle(triangle=unit_triangle(), rotate=1, rotate_origin=[0,0]):
+    # rotate it -- around the origin (Modify this to rotate around any specified point)
+    # this will be no "general" rotation, but instead it will be specifically a quarter turn
+    # it will be a quarter turn multiplied by 1, 2 or 3.
+    # First a single quarter turn
+    if rotate_origin == [0,0]:
+        if rotate==1:
+            return [
+                [triangle[0][1] * -1, triangle[0][0] * 1],
+                [triangle[1][1] * -1, triangle[1][0] * 1],
+                [triangle[2][1] * -1, triangle[2][0] * 1]
+                ]
+        elif rotate==2:
+            return rotate_triangle(rotate_triangle(triangle, 1, rotate_origin), 1, rotate_origin)
+        elif rotate==3:
+            return rotate_triangle(rotate_triangle(triangle, 1, rotate_origin), 2, rotate_origin)
+        else:
+            # do nothing to the input triangle; probably this should be flagged in a log
+            return triangle
+    else:
+        # translate the object to the origin by inverting the rotate rotate_origin
+        # rotate around the origin
+        # translate the object back to it's position by undoing the previous translattion
+        return translate_triangle(rotate_triangle(translate_triangle(triangle, [-rotate_origin[0], -rotate_origin[1]]), rotate), rotate_origin)
+
+
+T=[[5,5],[100, 5],[5, 100]]
+render_triangle(T)
+render_triangle(rotate_triangle(dilate_triangle(T, [1.3,1.1]), 1, [120, 120]))
+render_triangle(rotate_triangle(dilate_triangle(T, [1.6,1.2]), 2, [120, 120]))
+render_triangle(rotate_triangle(dilate_triangle(T, [1.9,1.3]), 3, [120, 120]))
+
+rotate_direction = raw_input('R-right, L-left')
+
+if rotate_direction in ['R', 'r']:
+    print('Right.')
+elif rotate_direction in ['L', 'l']:
+    print('Left')
+else:
+    print('wrong')
+
+"""
+###
+print(unit_triangle())
+print("1:", unit_triangle()[0])
+print("2:", unit_triangle()[1])
+print("3:", 100*unit_triangle()[2][0], 100*unit_triangle()[2][1])
+print("The unit Triangle,", unit_triangle())
+print("The unit Triangle, rotated 1 quarter turns: ", rotate_triangle(unit_triangle(), 1, [0,0]))
+print("The unit Triangle, rotated 2 quarter turns: ", rotate_triangle(unit_triangle(), 2, [0,0]))
+print("The unit Triangle, rotated 3 quarter turns: ", rotate_triangle(unit_triangle(), 3, [0,0]))
 render_triangle([[5,5],[100, 5],[5, 100]])
 
 # I would like to render many random triangles.
@@ -74,9 +111,9 @@ seed(1)
 def totally_random():
     for i in range(10):
         render_triangle([
-            [randint(0, canvas_width), randint(0, canvas_height)], 
-            [randint(0, canvas_width), randint(0, canvas_height)], 
-            [randint(0, canvas_width), randint(0, canvas_height)]]) 
+            [randint(0, canvas_width), randint(0, canvas_height)],
+            [randint(0, canvas_width), randint(0, canvas_height)],
+            [randint(0, canvas_width), randint(0, canvas_height)]])
 
 
 def arbitrary_xforms():
@@ -89,8 +126,10 @@ def arbitrary_xforms():
 
 # totally_random()
 arbitrary_xforms()
+###
+"""
 
 # So that's the random triangles. But what about some other triangles. Less random?
-# Starting with a single, small right-angle triangle in the middle. 
+# Starting with a single, small right-angle triangle in the middle.
 
 canvas.mainloop()
