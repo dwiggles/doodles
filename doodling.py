19b6be4789d23b98b3b514e9030d602ef7c3176d
Dan Wigglesworth <dwiggles@kewbeach.ca>
2019-08-18 15:29:36 -0400

    Imrpoved UI controls
    The sliders make more sense now, including intial conditions

--- doodling.py
+++ doodling.py
@@ -6,44 +6,63 @@ import random as rnd
 class Application(tk.Frame):
 
     canvas_width=500
-    canvas_height=canvas_width
+    #canvas_height=canvas_width
+    canvas_height=400
     canvas_centre = [canvas_width/2, canvas_height/2]
 
-    slider_dilation_start = 1000
-    slider_dilation_end = 2000
+
+    slider_dilation_start = 0
+    slider_dilation_end = 2
     slider_dilation_length=slider_dilation_end-slider_dilation_start
 
-    slider_initital_size_start= 2
-    slider_initial_size_end = 200
+    slider_dilation_denominator=2
+
+    slider_initital_size_start= 1
+    slider_initial_size_end = 100
     slider_initial_size_length=slider_initial_size_end-slider_initital_size_start
 
     slider_N_triangles_start= 1
-    slider_N_triangles_end = 500
+    slider_N_triangles_end = 100
     slider_N_triangles_length=slider_N_triangles_end-slider_N_triangles_start
 
     def __init__(self, master=None):
         super(Application, self).__init__(master)
         self.pack()
         self.createWidgets()
+        self.recursive_construction(2, 1, 50)
 
     def createWidgets(self):
         self.canvas = tk.Canvas(self,width=self.canvas_width, height=self.canvas_height)
         self.canvas.pack()
         # Switching Mac from "Dark Mode" to "Light Mode" "solves" a problem with the Button
         self.quitButton = tk.Button(self, text='Exit', command=self.quit)
-        self.quitButton.pack()
 
-        self.slider_dilation = tk.Scale(self, from_=self.slider_dilation_start, to=self.slider_dilation_end, orient="horizontal", length=self.canvas_width)
+        self.slider_dilation = tk.Scale(self, from_=self.slider_dilation_start, to=self.slider_dilation_end, orient="horizontal", length=self.canvas_width/2, label="Growth Rate", resolution=0.001)
+        self.slider_dilation.set(1)
+        #self.slider_dilation.bind("<B1-Motion>", self.response_dilation_high_res)
+        #self.slider_dilation.bind("<ButtonRelease-1>", self.response_dilation_high_res)
+        self.slider_dilation.bind("<B1-Motion>", self.response)
         self.slider_dilation.bind("<ButtonRelease-1>", self.response)
-        self.slider_dilation.pack()
 
-        self.slider_initial_size = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_initial_size_end, orient="horizontal", length=self.canvas_width)
+        self.slider_dilation_point1 = tk.Scale(self, from_=self.slider_dilation_start, to=self.slider_dilation_end, orient="horizontal", length=self.canvas_width/2, label="Growth Rate step=0.1", resolution=0.1)
+        #self.slider_dilation_point1.bind("<B1-Motion>", self.response_dilation_point1_res)
+        #self.slider_dilation_point1.bind("<ButtonRelease-1>", self.response_dilation_point1_res)
+
+        self.slider_initial_size = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_initial_size_end, orient="horizontal", length=self.canvas_width/2, label="Starting Size")
+        self.slider_initial_size.set(50)
+        self.slider_initial_size.bind("<B1-Motion>", self.response)
         self.slider_initial_size.bind("<ButtonRelease-1>", self.response)
-        self.slider_initial_size.pack()
 
-        self.slider_N_triangles = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_N_triangles_end, orient="horizontal", length=self.canvas_width)
+        self.slider_N_triangles = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_N_triangles_end, orient="horizontal", length=self.canvas_width/2, label="N")
+        self.slider_N_triangles.set(2)
+        self.slider_N_triangles.bind("<B1-Motion>", self.response)
         self.slider_N_triangles.bind("<ButtonRelease-1>", self.response)
+
+        self.slider_dilation.pack()
+        #self.slider_dilation_point1.pack()
+        self.slider_initial_size.pack()
         self.slider_N_triangles.pack()
+        self.quitButton.pack()
 
     def render_triangle(self, triangle):
         # render a triangle
@@ -121,38 +140,44 @@ class Application(tk.Frame):
        #  return the_points
        return [ A, B, C ]
 
+    def response_dilation_high_res(self, event):
+        size = self.slider_dilation.get()
+        self.response(event)
+
+    def response_dilation_point1_res(self, event):
+        size = self.slider_dilation_point1.get()
+        self.response(event)
+
+    # Attention future self: Should this (recursive_construction) really be defined INSIDE "response()"
+    #recursive_construction(N_triangles, size=size, initial_size_scalar=initial_size_scalar)
+    def recursive_construction(self, N, size, initial_size_scalar):
+        initial_size = [initial_size_scalar, initial_size_scalar]
+        dilation_factor = [(size/(self.slider_dilation_length/self.slider_dilation_denominator)), (size/(self.slider_dilation_length/self.slider_dilation_denominator))]
+
+        if N == 1:
+            self.canvas.delete("all")
+
+            T = self.dilate_triangle(self.unit_triangle(), initial_size)
+            T = self.translate_triangle(T, [self.canvas_centre[0]*1.5, self.canvas_centre[1]])
+            self.render_triangle(T)
+        else:
+            T=self.recursive_construction(N-1, size, initial_size_scalar)
+            if N % 2 == 0:  #even
+                T = self.dilate_triangle(T, dilation_factor, T[1])
+                T = self.reflect_triangle(T, offset=T[1][1], axis='X')
+            else: #odd
+                T = self.dilate_triangle(T, dilation_factor, T[2])
+                T = self.reflect_triangle(T, offset=T[2][0], axis='Y')
+            self.render_triangle(T)
+        # Notice that in the base case, the returned value is not processed.
+	    # Maybe indent this???
+        return T
+
     def response(self, event):
-        # This function is invoked by releasing the slider_dilation
         size = self.slider_dilation.get()
         initial_size_scalar = self.slider_initial_size.get()
-
-        def recursive_construction(N):
-            initial_size = [initial_size_scalar, initial_size_scalar]
-            dilation_factor = [(size/self.slider_dilation_length), (size/self.slider_dilation_length)]
-
-            if N == 1:
-                self.canvas.delete("all")
-
-                T = self.dilate_triangle(self.unit_triangle(), initial_size)
-                T = self.translate_triangle(T, [self.canvas_centre[0]*1.5, self.canvas_centre[1]])
-                self.render_triangle(T)
-            else:
-                T=recursive_construction(N-1)
-                if N % 2 == 0:  #even
-                    T = self.dilate_triangle(T, dilation_factor, T[1])
-                    T = self.reflect_triangle(T, offset=T[1][1], axis='X')
-                else: #odd
-                    T = self.dilate_triangle(T, dilation_factor, T[2])
-                    T = self.reflect_triangle(T, offset=T[2][0], axis='Y')
-                self.render_triangle(T)
-
-            # Notice that in the base case, the returned value is not processed.
-	        # Maybe indent this???
-            return T
-
-        #recursive_construction(500)
         N_triangles = self.slider_N_triangles.get()
-        recursive_construction(N_triangles)
+        self.recursive_construction(N_triangles, size, initial_size_scalar)
 
 app=Application()
 app.master.title('Doodling')
