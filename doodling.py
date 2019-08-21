3d6baa92289ff434c3792c06d9b7be26bb767204
Dan Wigglesworth <dwiggles@kewbeach.ca>
2019-08-21 13:48:32 -0400

    Generalize from Tile
    This renders triangles as a specific instance of a generalized "tile"

--- doodling.py
+++ doodling.py
@@ -18,9 +18,9 @@ class Application(tk.Frame):
     slider_initial_size_end = 130
     slider_initial_size_length=slider_initial_size_end-slider_initital_size_start
 
-    slider_N_triangles_start= 1
-    slider_N_triangles_end = 20
-    slider_N_triangles_length=slider_N_triangles_end-slider_N_triangles_start
+    slider_N_tiles_start= 1
+    slider_N_tiles_end = 20
+    slider_N_tiles_length=slider_N_tiles_end-slider_N_tiles_start
 
     def __init__(self, master=None):
         super(Application, self).__init__(master)
@@ -45,71 +45,62 @@ class Application(tk.Frame):
         self.slider_initial_size.bind("<B1-Motion>", self.response)
         self.slider_initial_size.bind("<ButtonRelease-1>", self.response)
 
-        self.slider_N_triangles = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_N_triangles_end, orient="horizontal", length=self.canvas_width*0.5, label="N")
-        self.slider_N_triangles.set(2)
-        self.slider_N_triangles.bind("<B1-Motion>", self.response)
-        self.slider_N_triangles.bind("<ButtonRelease-1>", self.response)
+        self.slider_N_tiles = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_N_tiles_end, orient="horizontal", length=self.canvas_width*0.5, label="N")
+        self.slider_N_tiles.set(2)
+        self.slider_N_tiles.bind("<B1-Motion>", self.response)
+        self.slider_N_tiles.bind("<ButtonRelease-1>", self.response)
 
         self.quitButton.pack()
         self.canvas.pack()
         self.slider_dilation.pack()
         self.slider_initial_size.pack()
-        self.slider_N_triangles.pack()
+        self.slider_N_tiles.pack()
 
-    def render_triangle(self, triangle):
-        self.canvas.create_line(triangle[0][0],triangle[0][1],triangle[1][0],triangle[1][1])
-        self.canvas.create_line(triangle[1][0],triangle[1][1],triangle[2][0],triangle[2][1])
-        self.canvas.create_line(triangle[2][0],triangle[2][1],triangle[0][0],triangle[0][1])
+    def render_tile(self, tile):
+        print(tile)
+        #self.canvas.create_polygon(tile, outline='#f11', fill='#1f1', width=2)
+        self.canvas.create_polygon(tile, outline='#000', fill='#888', width=1)
 
-    def translate_triangle(self, triangle, translation=[0,0]):
-        return [
-            [triangle[0][0] + translation[0], triangle[0][1] + translation[1]],
-            [triangle[1][0] + translation[0], triangle[1][1] + translation[1]],
-            [triangle[2][0] + translation[0], triangle[2][1] + translation[1]]
-            ]
+    def translate_tile(self, tile, translation=[0,0]):
+        tile[0::2] = [x + translation[0] for x in tile[0::2]]
+        tile[1::2] = [x + translation[1] for x in tile[1::2]]
+        return tile
 
-    def dilate_triangle(self, triangle, dilation=[1,1], dilate_origin=[0,0]):
+    def dilate_tile(self, tile, dilation=[1,1], dilate_origin=[0,0]):
         if dilate_origin == [0,0]:
-            return [
-                [triangle[0][0] * dilation[0], triangle[0][1] * dilation[1]],
-                [triangle[1][0] * dilation[0], triangle[1][1] * dilation[1]],
-                [triangle[2][0] * dilation[0], triangle[2][1] * dilation[1]]
-                ]
+            tile[0::2] = [x*dilation[0] for x in tile[0::2]]
+            tile[1::2] = [x*dilation[1] for x in tile[1::2]]
+            return tile
         else:
-            return self.translate_triangle(self.dilate_triangle(self.translate_triangle(triangle, [-dilate_origin[0], -dilate_origin[1]]), dilation), dilate_origin)
+            return self.translate_tile(self.dilate_tile(self.translate_tile(tile, [-dilate_origin[0], -dilate_origin[1]]), dilation), dilate_origin)
 
-    def reflect_triangle(self, triangle, offset=0, axis='X'):
+    def reflect_tile(self, tile, offset=0, axis='X'):
         if axis == 'X' or axis == 'x':
-            triangle = self.translate_triangle(triangle, [0,-offset])
-            triangle = [
-                [triangle[0][0], triangle[0][1] * -1],
-                [triangle[1][0], triangle[1][1] * -1],
-                [triangle[2][0], triangle[2][1] * -1]
-                ]
-            triangle = self.translate_triangle(triangle, [0,offset])
+            tile = self.translate_tile(tile, [0,-offset])
+            #tile = self.dilate_tile(tile, [1,-1], dilate_origin=[0,-offset])
+            tile[1::2] = [x * -1 for x in tile[1::2]]
+            tile = self.translate_tile(tile, [0,offset])
         else: # Assume "Y" intead
-            triangle = self.translate_triangle(triangle, [-offset,0])
-            triangle = [
-                [triangle[0][0] * -1, triangle[0][1]],
-                [triangle[1][0] * -1, triangle[1][1]],
-                [triangle[2][0] * -1, triangle[2][1]]
-                ]
-            triangle = self.translate_triangle(triangle, [offset,0])
-        return triangle
+            tile = self.translate_tile(tile, [-offset,0])
+            #tile = self.dilate_tile(tile, [-1,1], dilate_origin=[-offset,0])
+            tile[0::2] = [x * -1 for x in tile[0::2]]
+            tile = self.translate_tile(tile, [offset,0])
+        return tile
 
-    def unit_triangle(self):
-       A = [0, 0]
-       B = [1, 0]
-       C = [0, 1]
-       return [ A, B, C ]
-
-    def response_dilation_high_res(self, event):
-        size = self.slider_dilation.get()
-        self.response(event)
-
-    def response_dilation_point1_res(self, event):
-        size = self.slider_dilation_point1.get()
-        self.response(event)
+    def unit_tile(self,V):
+        # V is the number of vertices, an int
+        # Kind of assumes tile are regular polygons -- but not really
+        if V == 3:
+            A = [0, 0]
+            B = [1, 0]
+            C = [0, 1]
+            return A + B + C
+        if V == 4:
+            A = [0, 0]
+            B = [1, 0]
+            C = [0, 1]
+            D = [1, 1]
+        return  A + B + C + D
 
     def recursive_construction(self, N, size, initial_size_scalar):
         initial_size = [initial_size_scalar, initial_size_scalar]
@@ -117,24 +108,24 @@ class Application(tk.Frame):
 
         if N == 1:
             self.canvas.delete("all")
-            T = self.dilate_triangle(self.unit_triangle(), initial_size)
-            T = self.translate_triangle(T, [self.canvas_centre[0]*1.5, self.canvas_centre[1]])
+            T = self.dilate_tile(self.unit_tile(3), initial_size)
+            T = self.translate_tile(T, [self.canvas_centre[0], self.canvas_centre[1]])
         else:
             T=self.recursive_construction(N-1, size, initial_size_scalar)
             if N % 2 == 0:  #even
-                T = self.dilate_triangle(T, dilation_factor, T[1])
-                T = self.reflect_triangle(T, offset=T[1][1], axis='X')
+                T = self.dilate_tile(T, dilation_factor, [T[2],T[3]])
+                T = self.reflect_tile(T, offset=T[3], axis='X')
             else: #odd
-                T = self.dilate_triangle(T, dilation_factor, T[2])
-                T = self.reflect_triangle(T, offset=T[2][0], axis='Y')
-        self.render_triangle(T)
+                T = self.dilate_tile(T, dilation_factor, [T[4],T[5]])
+                T = self.reflect_tile(T, offset=T[4], axis='Y')
+        self.render_tile(T)
         return T
 
     def response(self, event):
         size = self.slider_dilation.get()
         initial_size_scalar = self.slider_initial_size.get()
-        N_triangles = self.slider_N_triangles.get()
-        self.recursive_construction(N_triangles, size, initial_size_scalar)
+        N_tiles = self.slider_N_tiles.get()
+        self.recursive_construction(N_tiles, size, initial_size_scalar)
 
 app=Application()
 app.master.title('Doodling')
