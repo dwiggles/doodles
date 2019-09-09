811d9ae2bc912a1f59d72610d507d833985d4c48
Dan Wigglesworth <dwiggles@kewbeach.ca>
2019-09-08 22:17:29 -0400

    Generalized Rotation Perfected
    Generalized, interactive rotation is perfected, including getting the rotation origin correct for the classical golden rectangle.

--- doodling.py
+++ doodling.py
@@ -6,12 +6,16 @@ import numpy as np
 class Application(tk.Frame):
 
     canvas_width=900
-    canvas_height=500
+    canvas_height=400
     canvas_centre = [canvas_width/2, canvas_height/2]
 
     slider_dilation_start = 0
     slider_dilation_end = 2
     slider_dilation_length=slider_dilation_end-slider_dilation_start
+
+    slider_rotation_start = 0
+    slider_rotation_end = 2*pi
+    slider_rotation_length=slider_rotation_end-slider_rotation_start
 
     slider_dilation_denominator=2
 
@@ -27,7 +31,7 @@ class Application(tk.Frame):
         super(Application, self).__init__(master)
         self.pack()
         self.createWidgets()
-        self.recursive_construction(2, 1, 50)
+        self.recursive_construction(N=2, size=1, rotate=pi, initial_size_scalar=50)
 
     def createWidgets(self):
         self.canvas = tk.Canvas(self,width=self.canvas_width, height=self.canvas_height)
@@ -40,6 +44,11 @@ class Application(tk.Frame):
         self.slider_dilation.bind("<ButtonRelease-1>", self.response)
 
         self.slider_dilation_point1 = tk.Scale(self, from_=self.slider_dilation_start, to=self.slider_dilation_end, orient="horizontal", length=self.canvas_width*0.5, label="Growth Rate step=0.1", resolution=0.1)
+
+        self.slider_rotation = tk.Scale(self, from_=self.slider_rotation_start, to=self.slider_rotation_end, orient="horizontal", length=self.canvas_width*0.9, label="Rotation", resolution=0.0001)
+        self.slider_rotation.set(pi)
+        self.slider_rotation.bind("<B1-Motion>", self.response)
+        self.slider_rotation.bind("<ButtonRelease-1>", self.response)
 
         self.slider_initial_size = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_initial_size_end, orient="horizontal", length=self.canvas_width*0.5, label="Starting Size")
         self.slider_initial_size.set(50)
@@ -54,17 +63,17 @@ class Application(tk.Frame):
         self.quitButton.pack()
         self.canvas.pack()
         self.slider_dilation.pack()
+        self.slider_rotation.pack()
         self.slider_initial_size.pack()
         self.slider_N_tiles.pack()
 
     def render_tile(self, tile):
         self.canvas.create_polygon(tile, outline='#000', fill='#888', width=1)
+        #self.canvas.create_polygon(tile, outline='#000', fill='', width=1)
 
     def translate_tile(self, tile, translation=[0,0]):
         tile[0::2] = [x + translation[0] for x in tile[0::2]]
         tile[1::2] = [x + translation[1] for x in tile[1::2]]
-        #tile[0::2] = [x + translation[0] for x in tile[0::2]]
-        #tile[1::2] = [x + translation[1] for x in tile[1::2]]
         return tile
 
     def dilate_tile(self, tile, dilation=[1,1], dilate_origin=[0,0]):
@@ -139,27 +148,43 @@ class Application(tk.Frame):
             dy = sin(pi*36/180)
             D = [E[0] + dx, E[1] + dy]
             return  A + B + C + D + E
+        if V == 6:
+            A = [0, 0]
+            B = [1, 0]
+            # The angle is 60
+            dx = cos(pi*60/180)
+            dy = sin(pi*60/180)
+            C = [1 + dx , 0 + dy]
+            F = [0 - dx, 0 + dy]
+            dx = cos(pi*60/180)
+            dy = sin(pi*60/180)
+            D = [C[0] - dx, C[1] + dy]
+            E = [F[0] + dx, F[1] + dy]
+            return A + B + C + D + E  + F
 
-    def recursive_construction(self, N, size, initial_size_scalar):
+    def recursive_construction(self, N, size, rotate, initial_size_scalar):
         initial_size = [initial_size_scalar, initial_size_scalar]
         dilation_factor = [(size/(self.slider_dilation_length/self.slider_dilation_denominator)), (size/(self.slider_dilation_length/self.slider_dilation_denominator))]
         if N == 1:
             self.canvas.delete("all")
-            T = self.dilate_tile(self.unit_tile(5), initial_size)
+            T = self.dilate_tile(self.unit_tile(4), initial_size)
             T = self.translate_tile(T, [self.canvas_centre[0], self.canvas_centre[1]])
             T = self.rotate_tile(T, rotate=2*3.1415926838/len(T), rotate_origin=[sum(T[0::2])/len(T)*2,sum(T[1::2])/len(T)*2])
         else:
-            T=self.recursive_construction(N-1, size, initial_size_scalar)
-            T = self.rotate_tile(T[2::]+T[0:2], rotate=2*3.1415926838/len(T), rotate_origin=[T[0],T[1]])
+            T=self.recursive_construction(N-1, size, rotate, initial_size_scalar)
+            #T = self.rotate_tile(T[2::]+T[0:2], rotate=+2*3.1415926838/len(T), rotate_origin=[T[0],T[1]])
+            #T = self.rotate_tile(T[2::]+T[0:2], rotate=rotate, rotate_origin=[T[0],T[1]])
+            T = self.rotate_tile(T[4::]+T[0:4], rotate=rotate, rotate_origin=[T[4],T[5]])
             T = self.dilate_tile(T,dilation_factor, [T[0],T[1]])
         self.render_tile(T)
         return T
 
     def response(self, event):
         size = self.slider_dilation.get()
+        rotate = self.slider_rotation.get()
         initial_size_scalar = self.slider_initial_size.get()
         N_tiles = self.slider_N_tiles.get()
-        self.recursive_construction(N_tiles, size, initial_size_scalar)
+        self.recursive_construction(N_tiles, size, rotate, initial_size_scalar)
 
 app=Application()
 app.master.title('Doodling')
