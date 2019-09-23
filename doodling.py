c5fd1a3849dcf6bf5c48b9a021f6e6503c443456
Dan Wigglesworth <dwiggles@kewbeach.ca>
2019-09-22 21:49:32 -0400

    so many changes
    - triangles are equilateral, now
    - arbitrary shape at polygon = 2

--- doodling.py
+++ doodling.py
@@ -10,18 +10,21 @@ class Application(tk.Frame):
     canvas_centre = [canvas_width/2, canvas_height/2]
 
     slider_dilation_start = 0
-    slider_dilation_end = 2
+    slider_dilation_end = 3
     slider_dilation_length=slider_dilation_end-slider_dilation_start
 
     slider_rotation_start = 0
     slider_rotation_end = 2*pi
     slider_rotation_length=slider_rotation_end-slider_rotation_start
 
-    slider_dilation_denominator=2
+    slider_dilation_denominator=3
 
     slider_initital_size_start= 1
     slider_initial_size_end = 300
     slider_initial_size_length=slider_initial_size_end-slider_initital_size_start
+
+    slider_tile_type_start=2
+    slider_tile_type_end=7
 
     slider_N_tiles_start= 1
     slider_N_tiles_end = 90
@@ -31,7 +34,7 @@ class Application(tk.Frame):
         super(Application, self).__init__(master)
         self.pack()
         self.createWidgets()
-        self.recursive_construction(N=2, size=1, rotate=pi, initial_size_scalar=50)
+        self.recursive_construction(N=2, size=1, rotate=pi, initial_size_scalar=50, tile_type=4)
 
     def createWidgets(self):
         self.canvas = tk.Canvas(self,width=self.canvas_width, height=self.canvas_height)
@@ -55,12 +58,18 @@ class Application(tk.Frame):
         self.slider_initial_size.bind("<B1-Motion>", self.response)
         self.slider_initial_size.bind("<ButtonRelease-1>", self.response)
 
+        self.slider_tile_type= tk.Scale(self, from_=self.slider_tile_type_start, to=self.slider_tile_type_end, orient="vertical", length=self.canvas_width*0.5, label="Polygon")
+        self.slider_tile_type.set(4)
+        self.slider_tile_type.bind("<B1-Motion>", self.response)
+        self.slider_tile_type.bind("<ButtonRelease-1>", self.response)
+
         self.slider_N_tiles = tk.Scale(self, from_=self.slider_initital_size_start, to=self.slider_N_tiles_end, orient="horizontal", length=self.canvas_width*0.5, label="N")
         self.slider_N_tiles.set(2)
         self.slider_N_tiles.bind("<B1-Motion>", self.response)
         self.slider_N_tiles.bind("<ButtonRelease-1>", self.response)
 
         self.quitButton.pack()
+        self.slider_tile_type.pack(side="left")
         self.canvas.pack()
         self.slider_dilation.pack()
         self.slider_rotation.pack()
@@ -69,6 +78,7 @@ class Application(tk.Frame):
 
     def render_tile(self, tile, skin):
         self.canvas.create_polygon(tile, outline='#000', fill=skin, width=1)
+        #self.canvas.create_polygon(tile, outline='#000', fill="", width=1)
 
     def translate_tile(self, tile, translation=[0,0]):
         tile[0::2] = [x + translation[0] for x in tile[0::2]]
@@ -117,56 +127,93 @@ class Application(tk.Frame):
     def unit_tile(self,V):
         # V is the number of vertices, an int
         # Kind of assumes tile are regular polygons -- but not really
+        # Equilateral triangle
         if V == 3:
             A = [0, 0]
             B = [1, 0]
-            C = [0, 1]
-            return A + B + C
+            # The angle is 60
+            dx = cos(pi*60/180)
+            dy = sin(pi*60/180)
+            C = [B[0] - dx , B[1] + dy]
+            return  A + B + C
+
         if V == 4:
             A = [0, 0]
             B = [1, 0]
             C = [1, 1]
             D = [0, 1]
             return  A + B + C + D
+
         if V == 5:
             A = [0, 0]
             B = [1, 0]
             # The angle is 180 less 108, ie 72
-            dx = cos(pi*72/180)
-            dy = sin(pi*72/180)
+            dx = cos(pi*(180-108)/180)
+            dy = sin(pi*(180-108)/180)
             C = [1 + dx , 0 + dy]
             E = [0 - dx, 0 + dy]
-            dx = cos(pi*36/180)
-            dy = sin(pi*36/180)
-            D = [E[0] + dx, E[1] + dy]
+            dx = cos(pi*(180-2*108)/180)
+            dy = sin(pi*(180-2*108)/180)
+            D = [E[0] + dx, E[1] - dy]
             return  A + B + C + D + E
+
         if V == 6:
             A = [0, 0]
             B = [1, 0]
-            # The angle is 60
-            dx = cos(pi*60/180)
-            dy = sin(pi*60/180)
-            C = [1 + dx , 0 + dy]
-            F = [0 - dx, 0 + dy]
-            dx = cos(pi*60/180)
-            dy = sin(pi*60/180)
+            # The angle is 180 less 60, 360/6
+            dx = cos(pi*(180-60)/180)
+            dy = sin(pi*(180-60)/180)
+            C = [1 - dx , 0 + dy]
+            F = [0 + dx, 0 + dy]
+            dx = cos(pi*(180-2*60)/180)
+            dy = sin(pi*(180-2*60)/180)
             D = [C[0] - dx, C[1] + dy]
             E = [F[0] + dx, F[1] + dy]
             return A + B + C + D + E  + F
 
-    def recursive_construction(self, N, size, rotate, initial_size_scalar):
+        if V == 7:
+            A = [0, 0]
+            B = [1, 0]
+            # The angle is 180 less 900/7
+            dx = cos(pi*(180-900/7)/180)
+            dy = sin(pi*(180-900/7)/180)
+            C = [1 + dx , 0 + dy]
+            G = [0 - dx, 0 + dy]
+            dx = cos(pi*(180-2*900/7)/180)
+            dy = sin(pi*(180-2*900/7)/180)
+            D = [C[0] - dx, C[1] - dy]
+            F = [G[0] + dx, G[1] - dy]
+            dx = cos(pi*(180-3*900/7)/180)
+            dy = sin(pi*(180-3*900/7)/180)
+            E = [D[0] + dx, D[1] + dy]
+            return A + B + C + D + E + F + G
+
+        # Could return any old shape, really. Like this one:
+        if V == 2:
+                A = [0, 0]
+                B = [1, 0]
+                C = [1, 1]
+                D = [0, 1]
+                # The angle is 60
+                dx = cos(pi*60/180)
+                dy = sin(pi*60/180)
+                E = [B[0] - dx , B[1] - dy]
+                return  A + E + B + C + D
+
+    def recursive_construction(self, N, size, rotate, initial_size_scalar, tile_type):
         initial_size = [initial_size_scalar, initial_size_scalar]
         dilation_factor = [(size/(self.slider_dilation_length/self.slider_dilation_denominator)), (size/(self.slider_dilation_length/self.slider_dilation_denominator))]
         if N == 1:
             self.canvas.delete("all")
-            T = self.dilate_tile(self.unit_tile(5), initial_size)
+            T = self.dilate_tile(self.unit_tile(tile_type), initial_size)
             T = self.translate_tile(T, [self.canvas_centre[0], self.canvas_centre[1]])
-            T = self.rotate_tile(T, rotate=2*3.1415926838/len(T), rotate_origin=[sum(T[0::2])/len(T)*2,sum(T[1::2])/len(T)*2])
+            # render a red tile (ie skin = '#F00')
             self.render_tile(T, skin='#F00')
         else:
-            T=self.recursive_construction(N-1, size, rotate, initial_size_scalar)
+            T = self.recursive_construction(N-1, size, rotate, initial_size_scalar, tile_type)
             T = self.rotate_tile(T[4::]+T[0:4], rotate=rotate, rotate_origin=[T[4],T[5]])
             T = self.dilate_tile(T, dilation_factor, [T[0],T[1]])
+            # render a blue tile (ie skin = '#F00')
             self.render_tile(T, skin='#00F')
         return T
 
@@ -174,8 +221,9 @@ class Application(tk.Frame):
         size = self.slider_dilation.get()
         rotate = self.slider_rotation.get()
         initial_size_scalar = self.slider_initial_size.get()
+        tile_type = self.slider_tile_type.get()
         N_tiles = self.slider_N_tiles.get()
-        self.recursive_construction(N_tiles, size, rotate, initial_size_scalar)
+        self.recursive_construction(N_tiles, size, rotate, initial_size_scalar, tile_type)
 
 app=Application()
 app.master.title('Doodling')
